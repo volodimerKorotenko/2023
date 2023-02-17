6aec4a9b8b0adfe9ab1fbeaa00d6137fcaa12e91
Kostya Voloskyi <113849083+Algor666@users.noreply.github.com>
2023-02-17 13:35:23 +0200

    Add files via upload

---
+++ DK21/Voloskyi Konstantin/HW1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "math.h"
+#include <windows.h>
+int main()
+{
+  SetConsoleCP(1251);
+  SetConsoleOutputCP(1251);
+  size1();
+  matrix1_elements();
+  size2 ();
+  matrix2_elements();
+multiple ();
+result ();
+return 0;
+}
---
+++ DK21/Voloskyi Konstantin/HW1/math.h
@@ -0,0 +1,15 @@
+#ifndef MATH_H
+#define MATH_H
+
+
+int m, n, p, q, c, d, k, sum ;
+  int first[10][10], second[10][10], multiply[10][10];
+
+void size1 ();
+void matrix1_elements ();
+void size2 ();
+void matrix2_elements ();
+void multiple ();
+void result ();
+
+#endif
