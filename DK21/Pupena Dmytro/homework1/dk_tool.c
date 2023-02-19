996b669bacef4c8445a2d6ef1a8966b6e4b9c78f
DmitroP <40754389+DmitroP@users.noreply.github.com>
2023-02-19 17:41:37 +0200

    Add files via upload

---
+++ DK21/Pupena Dmytro/homework1/dk_tool.c
@@ -0,0 +1,20 @@
+#include "dktool.h"
+#include <stdio.h>
+#include <stdlib.h>
+int create_matrix(int a, int b)
+{
+    do
+    {
+        do
+        {
+          matrix[a][b] = scanf("введіть значення для (%i %i)\n", d,c);
+          return matrix[a][b];
+           d=d+1;
+
+        } while (d<=b);
+        c=c+1;
+        d=1;
+    } while (c<=a);
+    return matrix[1][1],matrix[1][2],matrix[2][1],matrix[2][2];
+    
+}
---
+++ DK21/Pupena Dmytro/homework1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "dktool.h"
+#include <stdlib.h>
+int main(int argc, const char * argv[])
+{
+    create_matrix(2,2);
+   printf("%i\n", matrix[1][2]);
+   return 0;
+}
