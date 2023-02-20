22e84cbd493d661505fc9da52f45929566383047
voyager556231 <vovls5433@gmail.com>
2023-02-20 03:02:55 +0200

    To Be Continued

--- DK21/Vova Laskarzhevskyy/HW1/dk_tool.c
+++ DK21/Vova Laskarzhevskyy/HW1/dk_tool.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 #include "dk_tool.h"
+#define SIZE_COND 16
 
-void () 
+void input_number()
 {
-        
-}
+        char bin_num[SIZE_COND];
+        printf("number = %s ", bin_num);
+        int n = fgets(bin_num, 8, stdin);
+        if (n > 8) {
+                fprintf(stderr, "Error: wrong condition. Enter binary number length less then 8\n");
+                exit(EXIT_FAILURE);
 
+        } else {
+                printf("Correct\n");
+        }
+}
+void number_grade()
+{
+
+}
 
--- DK21/Vova Laskarzhevskyy/HW1/dk_tool.h
+++ DK21/Vova Laskarzhevskyy/HW1/dk_tool.h
@@ -1,7 +1,8 @@
 #ifndef DK_TOOL_H_INCLUDED
 
 #define DK_TOOL_H_INCLUDED
-
+int input_number();
 int number_grade(int s);
 
 #endif
+
