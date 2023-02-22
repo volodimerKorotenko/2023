9ca033bfd8fb264757b68d91144d78e4640ba5b0
voyager556231 <vovls5433@gmail.com>
2023-02-22 16:36:17 +0200

    The following functions were rewritten

--- DK21/Vova Laskarzhevskyy/HW1/dk_tool.h
+++ DK21/Vova Laskarzhevskyy/HW1/dk_tool.h
@@ -2,11 +2,11 @@
 
 #define DK_TOOL_H_INCLUDED
 
-char *allocate_binary_num(void);
-char *input_number(void);
-int number_grade(int a);
-void free_binary_num(char *binary_num);
-//int alg4conv_bin2dec(void);
+int *allocate_mem4num(void);
+char *allocate_mem4str(void);
+void free_mem4num(int *arr);
+void free_mem4str(char *str);
+void input_dec_num(int num);
+void alg4conv_bin2dec(int num, int *arr);
 
 #endif
-
