152524c1a83a9941f19126e0f84c3cccfc660f6c
Kostya Voloskyi <113849083+Algor666@users.noreply.github.com>
2023-02-27 12:33:54 +0200

    Update main.c

--- DK21/Voloskyi Konstantin/HW1/main.c
+++ DK21/Voloskyi Konstantin/HW1/main.c
@@ -1,15 +1,39 @@
+#include <time.h>
 #include <stdio.h>
-#include "math.h"
+#include <stdlib.h>
 #include <windows.h>
-int main()
+
+int main(void)
 {
-  SetConsoleCP(1251);
+    SetConsoleCP(1251);
   SetConsoleOutputCP(1251);
-  size1();
-  matrix1_elements();
-  size2 ();
-  matrix2_elements();
-multiple ();
-result ();
-return 0;
+    int size;
+    int** m1;
+    int** m2;
+    int** m3;
+
+    srand((int) time(NULL));
+
+    printf("Розмір матриць: ");
+    scanf("%zd", &size);
+
+    m1 = allocate_matrix(size);
+    m2 = allocate_matrix(size);
+    m3 = allocate_matrix(size);
+
+    generate(m1, size, -5, 5);
+    generate(m2, size, -5, 5);
+
+    dump(m1, size, "Перша матриця:");
+    dump(m2, size, "Друга матриця:");
+
+    multiply(m3, m1, m2, size);
+
+    dump(m3, size, "Результат:");
+
+    free_matrix(m3, size);
+    free_matrix(m2, size);
+    free_matrix(m1, size);
+
+    exit(0);
 }
