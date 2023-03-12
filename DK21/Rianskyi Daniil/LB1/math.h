93c5a34f238bd7532b1739f1fd67cb915b730075
DaniilRiansky <114438770+DaniilRiansky@users.noreply.github.com>
2023-03-12 14:19:04 +0200

    Update math.h

--- DK21/Rianskyi Daniil/LB1/math.h
+++ DK21/Rianskyi Daniil/LB1/math.h
@@ -4,6 +4,7 @@
 long sum_factorials (int start, int finish);
 long factorial (int number);
 void stop_stupid(int *num);
+void stop_stupid1(int *A, int *C);
 void stop_stupid2(int *num);
 void stop_stupid3(int *B, int *C);
 double q(int num1, int num2, int num3);
