bbb246142de4b968a668126cf0434f0c2ddcd416
DaniilRiansky <114438770+DaniilRiansky@users.noreply.github.com>
2023-03-18 15:36:32 +0200

    Update line.h

--- DK21/Rianskyi Daniil/LB2/line.h
+++ DK21/Rianskyi Daniil/LB2/line.h
@@ -4,11 +4,17 @@
 typedef struct node {
     int data;
     struct node *next;
+    struct node *prev;
 } Node;
 
 Node *create_node(int data);
+
 void append(Node **head_ref, int data);
+
 void print_list(Node *head);
+
+void delete_list(Node **head_ref);
+
 void stop_stupid2(int *n);
 
 #endif
