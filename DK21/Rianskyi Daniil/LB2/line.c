ba6fb40dcbddc574c5875912d6fe1282591fc180
DaniilRiansky <114438770+DaniilRiansky@users.noreply.github.com>
2023-03-18 15:36:04 +0200

    Update line.c

--- DK21/Rianskyi Daniil/LB2/line.c
+++ DK21/Rianskyi Daniil/LB2/line.c
@@ -6,14 +6,23 @@
 Node *create_node(int data) {
     Node *new_node = (Node *)malloc(sizeof(Node));
     new_node->data = data;
+    new_node->prev = NULL;
     new_node->next = NULL;
     return new_node;
 }
 
 void append(Node **head_ref, int data) {
     Node *new_node = create_node(data);
-    new_node->next = *head_ref;
-    *head_ref = new_node;
+    if (*head_ref == NULL) {
+        *head_ref = new_node;
+        return;
+    }
+    Node *current = *head_ref;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next = new_node;
+    new_node->prev = current;
 }
 
 void print_list(Node *head) {
@@ -22,6 +31,16 @@ void print_list(Node *head) {
         head = head->next;
     }
     printf("\n");
+}
+
+void delete_list(Node **head_ref) {
+    Node *current = *head_ref;
+    while (current != NULL) {
+        Node *temp = current->next;
+        free(current);
+        current = temp;
+    }
+    *head_ref = NULL;
 }
 
 void stop_stupid2(int *n) {
