4fb6face29453a565e2b241951143bea75daee3c
dk21-KMihaylo <125995123+dk21-KMihaylo@users.noreply.github.com>
2023-03-20 20:18:19 +0300

    Update list.h

--- DK21/Kiyanitsa Mihaylo/LabWork2/list.h
+++ DK21/Kiyanitsa Mihaylo/LabWork2/list.h
@@ -1,1 +1,14 @@
+#ifndef NODE_H
+#define NODE_H
 
+typedef struct node {
+    int data;
+    struct node *next;
+} Node;
+
+Node* createNode(int data);
+void insertNode(Node** head, Node* newNode);
+void sortList(Node* head);
+void printList(Node* head);
+
+#endif /* NODE_H */
