731cea970f1a8142fddffc0d8eab9ffcd0b12d64
dk21-KMihaylo <125995123+dk21-KMihaylo@users.noreply.github.com>
2023-03-20 20:18:49 +0300

    Update main.c

--- DK21/Kiyanitsa Mihaylo/LabWork2/main.c
+++ DK21/Kiyanitsa Mihaylo/LabWork2/main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "node.h"
 
+int main() {
+    Node* head = NULL;
+    int data, n;
+
+    printf("Enter the number of elements in the list: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &data);
+        Node* newNode = createNode(data);
+        insertNode(&head, newNode);
+    }
+    
+    sortList(head);
+
+    printf("Sorted list: ");
+    printList(head);
+
+    return 0;
+}
