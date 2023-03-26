375dfaaadbaa3661ce75407300889cb2893d133f
voyager556231 <vovls5433@gmail.com>
2023-03-26 15:36:45 +0300

    main.c has been added

--- DK21/Vova Laskarzhevskyy/LW2/main.c
+++ DK21/Vova Laskarzhevskyy/LW2/main.c
@@ -1,31 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "list_w.h"
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-	list_t *list_ptr = NULL; 
+	if (argc < 2 ) {
+		fprintf(stderr, "Error: Usage ./program word1 word2..\n");
+		exit(EXIT_FAILURE);
+	}
 
-	for (int i = 1; i < argc; ++i) {
-		node_ptr->word = strdup(argv[i]);
-		node_ptr->length = strlen(node_ptr->word);
-		add_word2list(list_ptr, node_ptr->word, node_ptr->length);
+	/*
+	char *tmp_word = NULL;
+	for (int i = 2; i < argc; ++i) {
+		//strncpy(tmp_word, argv[i], strlen(argv[i]));
+		if ((tmp_word[i] >= '!' && tmp_word[i] <= '/') 
+				|| (tmp_word[i] >= ':' && tmp_word[i] <= '@')) {
+			fprintf(stderr, "Error: enter only words\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+	*/
+	list_t *list_ptr = create_list(argv[1], strlen(argv[1]));
+	for (int i = 2; i < argc; ++i) {
+		add_word2list(list_ptr, argv[i], strlen(argv[i]));
 	}
 	sort_word_list(list_ptr);
-
-	node_t *current = list_ptr->head;
-	while (current != NULL) {
-		printf("%s \n", current->word);
-		current = current->next;
-	}
+	print_list(list_ptr);
 	max_word_list(list_ptr);
-
-	while (list_ptr->head != NULL) {
-		node_t *tmp_node = list_ptr; 
-		list_ptr = list_ptr->head;
-		free(tmp_node->word);
-		delete_list(list_ptr);
-	}
+	delete_list(list_ptr);
 	return 0;
 }
