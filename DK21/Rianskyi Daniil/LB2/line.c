e7c93dab8403e6b5d028fc9c1328baed25854175
DaniilRiansky <114438770+DaniilRiansky@users.noreply.github.com>
2023-03-26 14:16:20 +0300

    Update line.c

--- DK21/Rianskyi Daniil/LB2/line.c
+++ DK21/Rianskyi Daniil/LB2/line.c
@@ -2,13 +2,36 @@
 #include <stdlib.h>
 #include "line.h"
 
-
 Node *create_node(int data) {
     Node *new_node = (Node *)malloc(sizeof(Node));
     new_node->data = data;
     new_node->prev = NULL;
     new_node->next = NULL;
     return new_node;
+}
+
+void stop_stupid(int *n) {
+    while(scanf("%d", n) != 1 ) {
+        printf("Invalid input, try again: ");
+        while (getchar() != '\n') {}
+    }
+}
+
+
+void print_elements(int *n, int *data, Node **head){
+    for (int i = 0; i < *n; i++) {
+        printf("Enter element %d: ", i + 1);
+        stop_stupid(data);
+        append(head, *data);
+    }
+}
+
+void print_list(Node *head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
 }
 
 void append(Node **head_ref, int data) {
@@ -25,15 +48,28 @@ void append(Node **head_ref, int data) {
     new_node->prev = current;
 }
 
-void print_list(Node *head) {
-    while (head != NULL) {
-        printf("%d ", head->data);
-        head = head->next;
+
+
+void turns_list(Node **head_ref) {
+    if (*head_ref != NULL) {
+    Node *current = *head_ref;
+    Node *prev = NULL;
+    Node *next = NULL;
+
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        current->prev = next; 
+        prev = current;
+        current = next;
     }
-    printf("\n");
+    *head_ref = prev;
+}
+
 }
 
 void delete_list(Node **head_ref) {
+    turns_list(head_ref);
     Node *current = *head_ref;
     while (current != NULL) {
         Node *temp = current->next;
@@ -43,9 +79,5 @@ void delete_list(Node **head_ref) {
     *head_ref = NULL;
 }
 
-void stop_stupid2(int *n) {
-    while(scanf("%d", n) != 1 || *n <= 0) {
-        printf("Invalid input, try again: ");
-        while (getchar() != '\n') {}
-    }
-}
+
+
