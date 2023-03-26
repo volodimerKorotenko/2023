29c30be9e1eb132ee5fd7cc3568905dc03fa8c6a
voyager556231 <vovls5433@gmail.com>
2023-03-26 15:34:57 +0300

    A function declaration has been added

--- DK21/Vova Laskarzhevskyy/LW2/list_w.h
+++ DK21/Vova Laskarzhevskyy/LW2/list_w.h
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
 #ifndef  STRUCT_H
 #define  STRUCT_H
 typedef struct node {
@@ -13,11 +18,12 @@ typedef struct single_list {
 
 node_t *create_node(char *word, int length);
 void delete_node(node_t *node_ptr); 
-void print_node(node_t *node_ptr);
+//void print_node(node_t *node_ptr);
 
-list_t *create_list(void);
+list_t *create_list(char *word, int length);
+void print_list(list_t *list_ptr);
 void delete_list(list_t *list_ptr);
-void add_word2list(list_t *list_ptr, char *word, int length);
+char add_word2list(list_t *list_ptr, char *word, int length);
 void sort_word_list(list_t *list_ptr);
 void max_word_list(list_t *list_ptr);
 #endif
