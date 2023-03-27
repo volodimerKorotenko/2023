6ecbb3e1bdca9015aab25c712aad6d463b13b584
dk21-KMihaylo <125995123+dk21-KMihaylo@users.noreply.github.com>
2023-03-27 23:30:04 +0300

    Update main.c

--- DK21/Kiyanitsa Mihaylo/LabWork1/main.c
+++ DK21/Kiyanitsa Mihaylo/LabWork1/main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include "math1.h"
 
+int main() {
+    float a, b, c, q;
+    a = input_float("Введіть A: ");
+    b = input_float("Введіть B: ");
+    c = input_float("Введіть C: ");
+    
+    q = formula(a, b, c);
+    
+    printf("Q = %.2f\n", q);
+    
+    return 0;
+}
