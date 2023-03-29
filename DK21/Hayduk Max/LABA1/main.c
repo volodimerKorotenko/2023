2ff224e1127c38d6db36659806b05ba34527befa
Max Hayduk <73790260+haydukmax@users.noreply.github.com>
2023-03-29 08:52:52 +0300

    Update main.c

--- DK21/Hayduk Max/LABA1/main.c
+++ DK21/Hayduk Max/LABA1/main.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mathi.h"
+#include <math.h>
 
+int main(void)
+{
+    int A, B, C;
+    printf("Enter A B C: ");
+    scanf("%d %d %d", &A, &B, &C);
+    printf("A = %d;\nB = %d;\nC = %d;\n", A, B, C);
+
+    if (B == 0)
+    {
+        printf("Can't divide by 0");
+        exit(EXIT_FAILURE);
+    }
+
+    float bc_sqrt = sqrt(B + C);
+    float suma_ac = Suma(A, C);
+    float result = (bc_sqrt * A) / B + suma_ac;
+
+    printf("%f", result);
+
+    return 0;
+}
