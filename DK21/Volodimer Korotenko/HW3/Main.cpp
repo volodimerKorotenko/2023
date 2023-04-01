d8db3d591349ed1ceded4424dae7c96ed330b4ac
Volodimer-Kor <124707456+Volodimer-Kor@users.noreply.github.com>
2023-04-01 19:02:22 +0300

    Add files via upload

---
+++ DK21/Volodimer Korotenko/HW3/Main.cpp
@@ -0,0 +1,28 @@
+﻿#include "Geometrics.h"
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int Size = 0, Index = 0;
+	do {
+		Size = GetInt( 6, "Entri size " );
+	} while( Size < 1  );
+	Rectangle** RectangleArray = CreateRectangleArray( Size );
+
+	printf( "Create extra rectangle\n" );
+	Rectangle* Temp = CreateRectangle();
+	FillRectangle( Temp );
+	printf( "Index: %i\n", AddToEndArray( RectangleArray, Temp, Size ) );
+	FeguresOutput( RectangleArray, Size );
+	printf( "%f\n", CalcAreaArraies( RectangleArray , Size ) );
+
+	do {
+		Index = GetInt( 6, "Entri index " );
+	} while( Index < 0 );
+	printf( "Index: %i\n", AddAfterIndex( RectangleArray, Temp, Index, Size ) );
+	FeguresOutput( RectangleArray, Size );
+	printf( "%f\n", CalcAreaArraies( RectangleArray, Size ) );
+	FreeFiguresArray( RectangleArray , Size );
+
+	free( Temp );
+}
+
