4d5776a182d5e70cca547e52402954fcf8c1418e
voyager556231 <vovls5433@gmail.com>
2023-04-05 13:15:03 +0300

    Added circle_array

--- DK21/Vova Laskarzhevskyy/HW's/HW2/main.c
+++ DK21/Vova Laskarzhevskyy/HW's/HW2/main.c
@@ -1,5 +1,6 @@
 #include "circle.h"
 
+#define SIZE_ARR 3
 #define PI 3.14
 #define CIRCLE_LENGTH(r) (2*PI*r)
 #define AREA(r) (PI*r*r)
@@ -22,16 +23,21 @@ int main(int argc, char **argv)
 	int x = atoi(argv[1]);
 	int y = atoi(argv[2]);
 	int radius = atoi(argv[3]); 
+
 	if (radius < 0) {
 		fprintf(stderr, "Error: this circle doesn't exist\n");
 		exit(EXIT_FAILURE);
 	}
 
-	circle_t *circle_ptr = create_circle(x, y, radius);
-	print_circle(circle_ptr);
+	circle_t *circle_arr[SIZE_ARR];
+
+	for (int i = 0; i < SIZE_ARR; ++i) {
+		circle_arr[i] = create_circle(x, y, radius);
+		print_circle(circle_arr[i]);
+		delete_circle(circle_arr[i]);
+	}
 	printf("\ncircle length = %.2f;\n", CIRCLE_LENGTH(radius));
 	printf("circle area = %.2f;\n", AREA(radius));
-
-	delete_circle(circle_ptr);
 	return 0;
 }
+
