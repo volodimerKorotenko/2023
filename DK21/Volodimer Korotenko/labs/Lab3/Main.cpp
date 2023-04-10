6d9c9985c876b57a3e791add9f6a75cc9484382e
Volodimer-Kor <124707456+Volodimer-Kor@users.noreply.github.com>
2023-04-10 21:52:55 +0300

    Add files via upload

---
+++ DK21/Volodimer Korotenko/labs/Lab3/Main.cpp
@@ -0,0 +1,15 @@
+﻿#include "Trees.h"
+
+int main()
+{
+   Node* BinaryTree = 0;
+   int count = 0;
+   do {
+      count = GetInt( 6, "Count: " );
+   } while( count < 0 );
+   BinaryTree = TreeEntry( BinaryTree, count );
+   PrintTree( BinaryTree );
+   DeleteNode( BinaryTree, GetInt( 6, "Delete value: " ));
+   PrintTree( BinaryTree );
+   FreeTree( BinaryTree );
+}
