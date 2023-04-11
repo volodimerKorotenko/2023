baea4f2633be27eef5b5ba440ab1d2521de7a918
voyager556231 <vovls5433@gmail.com>
2023-04-11 20:04:44 +0300

    tree.h was added

--- DK21/Vova Laskarzhevskyy/LW's/LW3/tree.h
+++ DK21/Vova Laskarzhevskyy/LW's/LW3/tree.h
@@ -22,6 +22,8 @@ void delete_node(node_t *node_ptr);
 void delete_tree(tree_t *tree_ptr);
 void print_node(node_t *node_ptr);
 void print_tree(tree_t *tree_ptr);
+void delete_node_value(tree_t *tree_ptr, char data);
+void show_node(node_t *node_ptr);
 node_t *find_node(tree_t *tree_ptr, char data);
 void delete_node_value(tree_t *tree_ptr, char data);
 void add_data2tree(tree_t *tree_ptr, char data);
