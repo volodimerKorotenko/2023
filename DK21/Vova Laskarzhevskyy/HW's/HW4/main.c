18d2e095f457d875a6dd9bc4dab54f6924ce2620
voyager556231 <vovls5433@gmail.com>
2023-04-16 06:48:16 +0300

    main.c was modified

--- DK21/Vova Laskarzhevskyy/HW's/HW4/main.c
+++ DK21/Vova Laskarzhevskyy/HW's/HW4/main.c
@@ -1,31 +1,27 @@
 #include "list.h"
-
 int main(void)
 {
 	list_t *list_ptr = create_list();
-
-	add_node2list(list_ptr, 0.2);
+	add_node2list(list_ptr, 5);
 	print_list(list_ptr);
 
-	add_node2list_by_index(list_ptr, 1, 1.5);
+	add_node2list_by_index(list_ptr, 1, 6);
 	print_list(list_ptr);
 
-	add_node2list(list_ptr, 2.7);
+	add_node2list_by_index(list_ptr, 2, 4);
 	print_list(list_ptr);
 
-	add_node2list(list_ptr, 3.4);
+	add_node2list(list_ptr, 7);
 	print_list(list_ptr);
 
-
-	add_node2list(list_ptr, 1.9);
+	add_node2list(list_ptr, 8);
 	print_list(list_ptr);
 
-	/*
-	list_del_node_by_index(list_ptr, 1); 
+	add_node2list(list_ptr, 9);
 	print_list(list_ptr);
-	*/
 
-	//count_list(list_ptr);
+	list_del_node_by_index(list_ptr, 4); 
+	print_list(list_ptr);
 
 	delete_list(list_ptr);
 	return 0;
