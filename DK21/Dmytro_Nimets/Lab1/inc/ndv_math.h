72fe169cad3bdb75ec54c38069179014e092f4cf
dc-256 <114987549+dc-256@users.noreply.github.com>
2023-04-25 23:18:29 +0200

    Upload Lab1

---
+++ DK21/Dmytro_Nimets/Lab1/inc/ndv_math.h
@@ -0,0 +1,12 @@
+#ifndef NDV_MATH_H
+#define NDV_MATH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+double q(float a,float b,float c);
+long power(int base,int pow);
+long sum_range(int start,int end);
+long factorial(int base);
+
+#endif
