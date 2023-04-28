2cec66b3c5e02334b08a82f491f3e91233c7e82c
dk21-KMihaylo <125995123+dk21-KMihaylo@users.noreply.github.com>
2023-04-28 21:04:56 +0300

    Update trees.c

--- DK21/Kiyanitsa Mihaylo/HomeWork5/trees.c
+++ DK21/Kiyanitsa Mihaylo/HomeWork5/trees.c
@@ -1,1 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+// Структура для вузла дерева
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+// Функція для створення нового вузла дерева
+struct node* create_node(int data) {
+    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+    new_node->data = data;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
+
+// Функція для вставки нового вузла до дерева
+struct node* insert_node(struct node* root, int data) {
+    if (root == NULL) {
+        return create_node(data);
+    }
+    if (data < root->data) {
+        root->left = insert_node(root->left, data);
+    } else if (data > root->data) {
+        root->right = insert_node(root->right, data);
+    }
+    return root;
+}
+
+// Функція для видалення вузла з дерева
+struct node* delete_node(struct node* root, int data) {
+    if (root == NULL) {
+        return root;
+    }
+    if (data < root->data) {
+        root->left = delete_node(root->left, data);
+    } else if (data > root->data) {
+        root->right = delete_node(root->right, data);
+    } else {
+        if (root->left == NULL) {
+            struct node *temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            struct node *temp = root->left;
+            free(root);
+            return temp;
+        }
+        struct node *temp = root->right;
+        while (temp->left != NULL) {
+            temp = temp->left;
+        }
+        root->data = temp->data;
+        root->right = delete_node(root->right, temp->data);
+    }
+    return root;
+}
+
+// Функція для пошуку елемента у дереві
+struct node* search_node(struct node* root, int data) {
+    if (root == NULL || root->data == data) {
+        return root;
+    }
+    if (data < root->data) {
+        return search_node(root->left, data);
+    } else {
+        return search_node(root->right, data);
+    }
+}
+
+// Функція для виводу дерева на екран
+void print_tree(struct node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        print_tree(root->left);
+        print_tree(root->right);
+    }
+}
+
+// Функція для видалення всього дерева
+void delete_tree(struct node* root) {
+    if (root != NULL) {
+        delete_tree(root->left);
+        delete_tree(root->right);
+        free(root);
+    }
+}
