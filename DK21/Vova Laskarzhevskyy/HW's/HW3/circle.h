301cb681a0a54b0d8b46137da189b56ef8d878ad
voyager556231 <vovls5433@gmail.com>
2023-05-10 03:45:42 +0300

    Added files about the circle

---
+++ DK21/Vova Laskarzhevskyy/HW's/HW3/circle.h
@@ -0,0 +1,17 @@
+#include "point.h"
+
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+typedef struct circle {
+	point_t *o;
+	int radius;
+} circle_t;
+
+circle_t *create_circle(int x, int y, int radius);
+circle_t *new_point_circle(point_t *center, int radius);
+void delete_circle(circle_t *circle_ptr);
+void print_circle(circle_t *circle_ptr);
+
+#endif
+
