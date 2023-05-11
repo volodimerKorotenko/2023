0c6c31f8d3fb82a90315376a2714b69f0479a3fb
voyager556231 <vovls5433@gmail.com>
2023-05-11 16:28:54 +0300

    fix: added correct pointer

--- DK21/Vova Laskarzhevskyy/HW's/HW3/main.c
+++ DK21/Vova Laskarzhevskyy/HW's/HW3/main.c
@@ -10,12 +10,13 @@ int main(void)
 	printf("All circles in array:\n");
 	print_arr_circles(arr_ptr);
 
-	circle_ptr = create_circle(3, 2, 5);
-	add_circles_at_index(arr_ptr, circle_ptr, 3);
+	circle_t *circle_ptr2 = create_circle(3, 2, 5);
+	add_circles_at_index(arr_ptr, circle_ptr2, 3);
 
 	printf("All circles in array:\n");
 	print_arr_circles(arr_ptr);
 
+	delete_circle(circle_ptr2);
 	delete_circle(circle_ptr);
 	delete_arr_circles(arr_ptr);
 
